7dea30e3d14dddfb7fe0848a3d5c05a788381fdf
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2023-05-02 21:14:39 +0200

    getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,20 +1,51 @@
-const { normalize } = require("path");
+//const { normalize } = require("path");
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+
+  const dom = new JSDOM(htmlBody);
+
+  linkElements = dom.window.document.querySelectorAll("a");
+
+  // checks if elements of 'linkElements' are valid and - if they are - pushes them into 'urls'
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative url
+      try {
+        const urlObj = new URL(baseURL.concat(linkElement.href));
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("Error with relative URL: " + err.message);
+      }
+    } else {
+      // absolute url
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("Error with absolute URL: " + err.message);
+      }
+    }
+  }
+  return urls;
+}
 
 function normalizeURL(url) {
   // removes trailing slash and converts to lowercase
-  const urlArr = url.split(""); 
-  
+  const urlArr = url.split("");
+
   const urlObj = new URL(
-    urlArr[urlArr.length - 1] === "/" 
+    urlArr[urlArr.length - 1] === "/"
       ? urlArr.slice(0, -1).join("")
       : urlArr.join("")
   );
-  
+
   // strips protocol
   return urlObj.host.concat(urlObj.pathname);
 }
 
-
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -26,6 +26,73 @@ test("normalizeURL CAPs", () => {
   const input = "https://sub.DOMAIN.com/path/";
   const actual = normalizeURL(input);
   const expected = "sub.domain.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTML = `
+  <html>
+    <body>
+      <a href = "https://sub.domain.com/">
+        Visit Domain Blog!
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = 'https://sub.domain.com'
+  const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+  const expected = ["https://sub.domain.com/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTML = `
+  <html>
+    <body>
+      <a href = "/path/">
+        Visit Domain Blog Path!
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = 'https://sub.domain.com'
+  const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+  const expected = ["https://sub.domain.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputHTML = `
+  <html>
+    <body>
+      <a href = "https://sub.domain.com/path1/">
+        Visit Domain Blog Path1!
+      </a>
+      <a href = "/path2/">
+        Visit Domain Blog Path2!
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = 'https://sub.domain.com'
+  const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+  const expected = ["https://sub.domain.com/path1/", "https://sub.domain.com/path2/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTML = `
+  <html>
+    <body>
+      <a href = "invalid">
+        Invalid URL!
+      </a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = 'https://sub.domain.com'
+  const actual = getURLsFromHTML(inputHTML, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
 
